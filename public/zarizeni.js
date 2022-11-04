58a76d8a9d6983aa5895dd20faf35e5ebb79f7d1
Noxyyk <setunskyt@spseplzen.cz>
2022-11-04 11:34:05 +0100

    registrace

--- public/zarizeni.js
+++ public/zarizeni.js
@@ -1,4 +1,8 @@
+function init(){
 var user = localStorage.getItem("user"); //load logged user from local storage
 if (user !== null) {
-    //document.getElementById('html').innerHTML = "";
+  document.getElementById('html').innerHTML = "";
   }
+}
+login_btn()
+init()
