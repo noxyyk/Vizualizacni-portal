18309a5f0da349ac28f4442b3663117ac2d536bb
noxyyk <79714899+noxyyk@users.noreply.github.com>
2022-12-06 23:06:38 +0100

    jwt token, origin, /delete

--- index.js
+++ index.js
@@ -9,13 +9,20 @@ fetch = import("node-fetch");
 // initialize database
 const bcrypt = require("bcryptjs");
 let { DB } = require("mongquick");
-const db = new DB('mongodb+srv://Noxyyk:T4ir4t3xi3@rguis.zkcth.mongodb.net/uiotv?retryWrites=true&w=majority'/*process.env.MongoLogin*/);
+require('dotenv').config();
+const db = new DB(process.env.MongoLogin);
+// Security
+const jwt = require('jsonwebtoken');
 
 const avatars = ['https://i.imgur.com/2CXjyN6.png', 'https://i.imgur.com/7bwnZht.png', 'https://i.imgur.com/UmwVaRU.png', 'https://i.imgur.com/ZJT5vOm.png'];
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const originsAllowed = [
+  "http://localhost:5000",
+  "https://vizualizacni-portal.up.railway.app/"
+]
 /*FUNCTIONS*/
 function listsGetRandomItem(list, remove) {
   var x = Math.floor(Math.random() * list.length);
@@ -25,23 +32,31 @@ function listsGetRandomItem(list, remove) {
     return list[x];
   }
 }
+function createToken(username,user) {
+  return jwt.sign({iss: username,avatar: user.user.avatar,admin: user.user.admin, exp: Math.floor(Date.now() / 1000) + (14 * 86400) 
+}, process.env.JWTSECRET , { algorithm: 'HS256' })
+}
+
+// Routes
 app.post('/login', async function(req, res) {
   res.header("Content-Type", 'application/json');
-  if (await (db.has(req.body.username))) { //check if name exists in database
-    if (bcrypt.compareSync(req.body.password, (((await (db.get(req.body.username)))[String('user')])[String('password')]))) {
-      res.status(200).send({valid: true });
-    } else {
-      res.status(401).send({valid: false });
-    }
-  } else {
-    res.status(404).send({valid: false});
-  }
-})
+  if (!originsAllowed.includes(req.get('origin'))) return res.status(401).send({valid: false, response: "pokus o spuštění z neautorizovaného zdroje"})
+  if (!(await (db.has(req.body.username)))) return res.status(404).send({valid: false, response: "Uživatel s tímto jménem neexistuje"})
+  if (!(bcrypt.compareSync(req.body.password, (await (db.get(req.body.username))).user.password))) return res.status(401).send({valid: false, response: "Heslo se neshhoduje"});
+
+  var object = await (db.get(req.body.username))
+  var token = createToken(req.body.username,object)
+  if (token == 'undefined') return res.status(500).send({valid: false, response: "Nastala chyba, zkuste to znovu později"})
+      
+    res.status(200).send({valid: true, pfp: object.user.avatar, token: token});
+  });
 
 app.post('/register', async function(req, res) {
   res.header("Content-Type", 'application/json');
-  if (!(await (db.has(req.body.username)))) { //check if name doesn´t exists in the database
-    object = {
+  if (!originsAllowed.includes(req.get('origin'))) return res.status(401).send({valid: false, response: "pokus o spuštění z neautorizovaného zdroje"})
+  if ((await (db.has(req.body.username)))) return res.status(409).send({valid: false, response: "Uživatel s tímto jménem již existuje"});
+   
+  object = {
       "user": {
         "password": (bcrypt.hashSync(req.body.password, 10)),
         "avatar": listsGetRandomItem(avatars, false),
@@ -56,34 +71,55 @@ app.post('/register', async function(req, res) {
     db.set(req.body.username, object);
     db.add('ID', 1);
     res.status(201).send({valid: true});
-  } else {
-    res.status(409).send({valid: false});
-  }
 })
 
 app.post('/change', async function(req, res) {
   res.header("Content-Type", 'application/json');
+  if (!originsAllowed.includes(req.get('origin'))) return res.status(401).send({valid: false, response: "pokus o spuštění z neautorizovaného zdroje"})
  if (await ((db.has(req.body.username)))) var object = await (db.get(req.body.username)); 
   switch (req.body.type) {
     case 'password':
-      if (object.user.password == req.body.password || req.body.password == req.body.username) return res.status(401).send({valid: false});
+      if (bcrypt.compareSync(req.body.password, object.user.password)) return res.status(401).send({valid: false, response: "zvolené heslo je stejné jako předtím"});
+      if (req.body.password == req.body.username) return res.status(401).send({valid: false, response: "zvolené heslo nesmí být stejné jako uživatelské jméno"});
       object.user.password = (bcrypt.hashSync(req.body.password, 10));
       db.set(req.body.username, object);
             res.status(200).send({valid: true});
     break;
     case 'name':
-      if (req.body.username == req.body.name || (await (db.has(req.body.name)))) return res.status(401).send({valid: false});
+      if (req.body.username == req.body.name) return res.status(401).send({valid: false, response: "zvolené jméno je stejné jako předtím"});
+       if (await (db.has(req.body.name))) return res.status(401).send({valid: false, response: "uživatel s tímto jménem již existuje"});
       db.set(req.body.name, object);
       db.delete(req.body.username);
       res.status(200).send({valid: true});
      break;
      case 'check':
-      if (await (!(db.has(req.body.username)))) return res.status(409).send({valid: false});
+      if (await (!(db.has(req.body.username)))) return res.status(409).send({valid: false, response: "uživatel neexistuje"});
       res.status(200).send({valid: true});
      default:
-      res.status(400).send({valid: false});
+      res.status(400).send({valid: false, response: "neznámý typ"});
     }
     })
+
+app.post('/delete', async function(req, res) {
+      res.header("Content-Type", 'application/json');
+      if (!originsAllowed.includes(req.get('origin'))) return res.status(401).send({valid: false, response: "pokus o spuštění z neautorizovaného zdroje"})
+      if (!(await (db.has(req.body.username)))) return res.status(409).send({valid: false, response: "Uživatel s tímto jménem nexistuje"});
+      db.delete(req.body.username);
+      res.status(200).send({valid: true});
+    })
+    app.post('/verify', async function(req, res) {
+      res.header("Content-Type", 'application/json');
+      if (!originsAllowed.includes(req.get('origin'))) return res.status(401).send({valid: false, response: "pokus o spuštění z neautorizovaného zdroje"})
+      jwt.verify(req.body.token, process.env.JWTSECRET, function(err, decoded) {
+
+        if (err) return res.status(401).send({valid: false, response: err})
+        if (decoded == 'undefined') return res.status(500).send({valid: false, response: "Nastala chyba, zkuste to znovu později"})
+          
+        res.status(200).send({valid: true, pfp: decoded.avatar, user: decoded.iss});
+      });
+
+      });
+
 app.use(express.static(__dirname + "/public"));
 
 app.listen(PORT);
