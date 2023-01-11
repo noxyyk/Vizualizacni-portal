4bff7ff36dfcaf08553887bcca5bf3c5c2217f9f
noxyyk <79714899+noxyyk@users.noreply.github.com>
2023-01-11 16:38:01 +0100

    e

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 5000;
+const bodyParser = require('body-parser');
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + "/public"));
 app.listen(PORT, () => {
